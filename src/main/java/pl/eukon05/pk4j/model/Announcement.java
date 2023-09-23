d3d21084d8e0103e8415467c4d527c7e0c71ee8d
Łukasz Konieczny <lukimaluki@gmail.com>
2023-09-23 11:50:19 +0200

    - Variable declarations and definitions are now one-line in Announcement fromElement() method

--- src/main/java/pl/eukon05/pk4j/model/Announcement.java
+++ src/main/java/pl/eukon05/pk4j/model/Announcement.java
@@ -11,20 +11,13 @@ public record Announcement(long id, String title, String content, String priorit
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
     public static Announcement fromElement(Element element) {
-        long id;
-        String title;
-        String content;
-        String priority;
-        String author;
-        LocalDateTime lastModified;
-
-        id = Long.parseLong(element.attr("onclick").replaceAll("\\D", ""));
+        long id = Long.parseLong(element.attr("onclick").replaceAll("\\D", ""));
         Elements rows = element.select("td");
-        title = rows.get(1).text();
-        content = rows.get(2).select("span").get(0).attr("title");
-        priority = rows.get(4).text();
-        author = rows.get(5).text();
-        lastModified = LocalDateTime.parse(rows.get(6).text(), DATE_TIME_FORMATTER);
+        String title = rows.get(1).text();
+        String content = rows.get(2).select("span").get(0).attr("title");
+        String priority = rows.get(4).text();
+        String author = rows.get(5).text();
+        LocalDateTime lastModified = LocalDateTime.parse(rows.get(6).text(), DATE_TIME_FORMATTER);
 
         return new Announcement(id, title, content, priority, author, lastModified);
     }
