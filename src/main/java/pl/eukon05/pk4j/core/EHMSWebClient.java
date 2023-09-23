67995a109c53512aaec34fa5a3200f13fe47d04a
Łukasz Konieczny <lukimaluki@gmail.com>
2023-09-23 12:01:04 +0200

    - Moved strings required for checking if login was successful to constants in EHMSWebClient - Renamed logger to LOGGER in EHMSWebClient, as it is a constant - Renamed the AUTH_CHECK constant to a more descriptive NOT_LOGGED_IN in EHMSWebClient

--- src/main/java/pl/eukon05/pk4j/core/EHMSWebClient.java
+++ src/main/java/pl/eukon05/pk4j/core/EHMSWebClient.java
@@ -17,40 +17,42 @@ class EHMSWebClient {
     private EHMSWebClient() {
     }
 
-    private static final String AUTH_CHECK = "Logowanie do systemu";
-    private static final Logger logger = LoggerFactory.getLogger(EHMSWebClient.class);
+    private static final String NOT_LOGGED_IN = "Logowanie do systemu";
+    private static final String RATE_LIMITED = "Przepisz kod z obrazka";
+    private static final String ALREADY_LOGGED_IN = "Wykryto podwójne zalogowanie";
+    private static final Logger LOGGER = LoggerFactory.getLogger(EHMSWebClient.class);
 
     static Document getRequest(EHMSUrl url, EHMSUser user) throws IOException {
-        logger.debug("Trying to retrieve data from {} for user {} ...", url.value(), user.login);
+        LOGGER.debug("Trying to retrieve data from {} for user {} ...", url.value(), user.login);
         Connection.Response response = Jsoup.connect(url.value()).cookies(user.cookies).execute();
 
         if (response.statusCode() != 200) {
-            logger.error("EHMS returned an unexpected status code: {}", response.statusCode());
+            LOGGER.error("EHMS returned an unexpected status code: {}", response.statusCode());
             throw new EHMSException(response.statusCode());
         }
 
         Document responseBody = response.parse();
 
-        if (!responseBody.getElementsContainingText(AUTH_CHECK).isEmpty()) {
-            logger.debug("User {} didn't have an active session, trying to log in...", user.login);
+        if (!responseBody.getElementsContainingText(NOT_LOGGED_IN).isEmpty()) {
+            LOGGER.debug("User {} didn't have an active session, trying to log in...", user.login);
             login(user);
             return getRequest(url, user);
         }
 
-        logger.debug("Successfully retrieved data from {}, for user {}", url.value(), user.login);
+        LOGGER.debug("Successfully retrieved data from {}, for user {}", url.value(), user.login);
         return responseBody;
     }
 
     private static void login(EHMSUser user) throws IOException {
-        logger.debug("Trying to retrieve a login form from EHMS...");
+        LOGGER.debug("Trying to retrieve a login form from EHMS...");
         Connection.Response response = Jsoup.connect(EHMSUrl.BASE.value()).execute();
 
         if (response.statusCode() != 200) {
-            logger.error("EHMS returned an unexpected status code: {}", response.statusCode());
+            LOGGER.error("EHMS returned an unexpected status code: {}", response.statusCode());
             throw new EHMSException(response.statusCode());
         }
 
-        logger.debug("Successfully retrieved the login form");
+        LOGGER.debug("Successfully retrieved the login form");
         Document document = response.parse();
 
         Map<String, String> cookies = response.cookies();
@@ -63,22 +65,22 @@ class EHMSWebClient {
 
         Map<String, String> data = Map.of(loginForm, user.login, passForm, user.password, "log_form", "yes", "counter", counter);
 
-        logger.debug("Trying to log in as user {} ...", user.login);
+        LOGGER.debug("Trying to log in as user {} ...", user.login);
 
         Document result = Jsoup.connect(EHMSUrl.BASE.value()).data(data).cookies(cookies).post();
 
-        if (result.getElementsContainingText(AUTH_CHECK).isEmpty()) {
-            logger.debug("Successfully logged in as user {}", user.login);
+        if (result.getElementsContainingText(NOT_LOGGED_IN).isEmpty()) {
+            LOGGER.debug("Successfully logged in as user {}", user.login);
             user.cookies = cookies;
         } else {
-            if (!result.getElementsContainingText("Przepisz kod z obrazka").isEmpty()) {
-                logger.warn("User {} got rate-limited! Please wait or solve the captcha on a different device before trying to log in again!", user.login);
+            if (!result.getElementsContainingText(RATE_LIMITED).isEmpty()) {
+                LOGGER.warn("User {} got rate-limited! Please wait or solve the captcha on a different device before trying to log in again!", user.login);
                 throw new RateLimitExceededException(user.login);
-            } else if (!result.getElementsContainingText("Wykryto podwójne zalogowanie").isEmpty()) {
-                logger.warn("User {} is already logged in on another device! Please logout from other devices before trying to log in again!", user.login);
+            } else if (!result.getElementsContainingText(ALREADY_LOGGED_IN).isEmpty()) {
+                LOGGER.warn("User {} is already logged in on another device! Please logout from other devices before trying to log in again!", user.login);
                 throw new UserAlreadyLoggedInException(user.login);
             } else {
-                logger.warn("Authentication for user {} failed, are the login details correct?", user.login);
+                LOGGER.warn("Authentication for user {} failed, are the login details correct?", user.login);
                 throw new IllegalArgumentException(String.format("Authentication failed for user %s, are the login details correct?", user.login));
             }
         }
